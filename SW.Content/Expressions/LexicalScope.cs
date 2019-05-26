4e41e36e4bf0223843fa1a7a961081d2a41b5be7
yaser <yaser@simplify9.com>
2019-05-26 16:43:52 +0300

    Content Expression interface

---
+++ SW.Content/Expressions/LexicalScope.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.Content.Expressions
+{
+    public class LexicalScope
+    {
+        readonly ContentObject _data;
+
+        public LexicalScope(ContentObject data)
+        {
+            _data = data;
+        }
+    }
+}
